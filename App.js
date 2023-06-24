df92912e125f0cb2412e4945f43fc86a7503f10d
Lee sang Jik <sangjik9206@gmail.com>
2023-06-24 14:40:51 +0900

    Banner Image
    Maplestory M

--- App.js
+++ App.js
@@ -1,21 +1,16 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { NavigationContainer } from "@react-navigation/native";
 import Root from "./navigation/Root";
 
+const queryClient = new QueryClient();
+
 export default function App() {
   return (
-    <NavigationContainer>
-      <Root />
-    </NavigationContainer>
+    <QueryClientProvider client={queryClient}>
+      <NavigationContainer>
+        <Root />
+      </NavigationContainer>
+    </QueryClientProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
--- screens/home.js
+++ screens/home.js
@@ -1,11 +1,58 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Dimensions, FlatList, StyleSheet, Text, View } from "react-native";
+import Swiper from "react-native-swiper";
+import { useQuery } from "@tanstack/react-query";
+import Loader from "../components/Loader";
+import styled from "styled-components/native";
+
+const BgImg = styled.Image``;
+
+const { height: SCREEN_HEIGHT } = Dimensions.get("window");
 
 const Home = () => {
-  return (
-    <View>
-      <Text>Hi</Text>
-    </View>
+  const { data } = useQuery(["MaplestoryM", "BannerImg"], () =>
+    fetch(
+      "https://forum.nexon.com/api/v1/community/maplestorym?alias=maplestorym&countryCode=kr"
+    ).then((response) => response.json())
+  );
+
+  return !data ? (
+    <Loader />
+  ) : (
+    <FlatList
+      ListHeaderComponent={
+        <>
+          <Swiper
+            horizontal
+            containerStyle={{
+              width: "100%",
+              height: SCREEN_HEIGHT / 3.5,
+              marginBottom: 40,
+            }}
+            loop
+            autoplay
+            autoplayTimeout={3.5}
+            showsButtons={false}
+            showsPagination={false}
+          >
+            {data?.communityHome?.webPlace?.mobileBanners
+              ? data?.communityHome?.webPlace?.mobileBanners.map(
+                  (img, index) => (
+                    <View style={{ flex: 1 }} key={index}>
+                      <BgImg
+                        style={StyleSheet.absoluteFill}
+                        key={img.createDate}
+                        source={{ uri: img.webImageUrl }}
+                        resizeMode="stretch"
+                      />
+                    </View>
+                  )
+                )
+              : null}
+          </Swiper>
+        </>
+      }
+    ></FlatList>
   );
 };
 
